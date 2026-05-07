d390c61adc802b62f48adfb4cf2685c85006e330
Joao Marcos Marques De Oliveira <jmoliveira9@topazevolution.com>
2026-05-07 11:21:40 -0300

    feat: adds flatlist for better control of things

--- links-rocketseat/src/app/index/index.tsx
+++ links-rocketseat/src/app/index/index.tsx
@@ -3,9 +3,8 @@ import { MaterialIcons } from "@expo/vector-icons";
 
 import { styles } from "./styles";
 import { colors } from "@/styles/color";
-import { categories } from "@/utils/categories";
 
-import Category from "@/components/category";
+import Categories from "@/components/categories";
 
 export default function Index() {
   return (
@@ -16,21 +15,7 @@ export default function Index() {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
-      <Category
-        name={categories[0].name}
-        icon={categories[0].icon}
-        isSelected
-      />
-      <Category
-        name={categories[1].name}
-        icon={categories[1].icon}
-        isSelected={false}
-      />
-      <Category
-        name={categories[2].name}
-        icon={categories[2].icon}
-        isSelected={false}
-      />
+      <Categories />
     </View>
   );
 }
