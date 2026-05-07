610ff2f630176fdc51073de0c481f7e3cc0054da
Joao Marcos Marques De Oliveira <jmoliveira9@topazevolution.com>
2026-05-07 15:14:38 -0300

    feat: using modal for details

--- links-rocketseat/src/app/index/index.tsx
+++ links-rocketseat/src/app/index/index.tsx
@@ -1,4 +1,11 @@
-import { Text, View, Image, TouchableOpacity, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+} from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import { styles } from "./styles";
@@ -6,6 +13,7 @@ import { colors } from "@/styles/color";
 
 import { Link } from "@/components/link";
 import Categories from "@/components/categories";
+import React from "react";
 
 export default function Index() {
   return (
@@ -32,6 +40,25 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.modalLinkName}>GitHub</Text>
+
+            <Text style={styles.modalUrl}>https://github.com/Burrg-cod</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
