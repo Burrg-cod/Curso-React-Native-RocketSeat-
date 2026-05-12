8d476a6b17cf600033556f9b4e3cfa30246eade4
Joao Marcos Marques De Oliveira <jmoliveira9@topazevolution.com>
2026-05-12 10:02:11 -0300

    feat: adds icons in the modals

--- links-rocketseat/src/app/index/index.tsx
+++ links-rocketseat/src/app/index/index.tsx
@@ -12,6 +12,7 @@ import { styles } from "./styles";
 import { colors } from "@/styles/color";
 
 import { Link } from "@/components/link";
+import { Option } from "@/components/options";
 import Categories from "@/components/categories";
 import React from "react";
 
@@ -56,6 +57,10 @@ export default function Index() {
             <Text style={styles.modalLinkName}>GitHub</Text>
 
             <Text style={styles.modalUrl}>https://github.com/Burrg-cod</Text>
+            <View style={styles.modalFooter}>
+              <Option name="Excluir" icon="delete" variant="secondary" />
+              <Option name="Abrir" icon="language" />
+            </View>
           </View>
         </View>
       </Modal>
